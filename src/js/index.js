52bc4f3ef5c4f3f1142c786428d92ad7546234cb
jaridwarren <jaridwarren@gmail.com>
2018-08-23 22:51:57 -0700

    added refresh button, randomized sounds on press

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  var audio = new Audio("../../assets/simpsons-intro.wav");
-  // audio.play();
+  const introAudio = new Audio("../../assets/sounds/simpsons-intro.wav");
+  introAudio.play();
+  const soundClips = ["doh", "excellent", "woohoo", "yes"];
+  ajaxCall();
+  $(".button").click(() => {
+    let soundIndex = Math.floor(Math.random() * 3.99);
+    let buttonAudio = new Audio(
+      `../../assets/sounds/${soundClips[soundIndex]}.wav`
+    );
+    buttonAudio.play();
+    ajaxCall();
+  });
+});
 
+function ajaxCall() {
   $.ajax({
     method: "get",
     url: "https://thesimpsonsquoteapi.glitch.me/quotes"
@@ -10,9 +22,8 @@ $(document).ready(function() {
       $(".quote").html(`"${data[0].quote}"`);
       $(".author").html(`&mdash; ${data[0].character}`);
       $(".character").attr("src", data[0].image);
-      console.log(data);
     })
     .fail(function(err) {
-      console.log(err);
+      alert("Something went wrong.");
     });
-});
+}
